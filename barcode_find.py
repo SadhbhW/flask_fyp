11b841a0823db9ab39d0f8f2856c23a1f0f1149e
SadhbhW <58007797+SadhbhW@users.noreply.github.com>
2020-04-10 15:56:20 +0100

    Add files via upload

--- barcode_find.py
+++ barcode_find.py
@@ -1,32 +1,48 @@
 from pyzbar import pyzbar
 import cv2
-# requests
-
-def activate(data):
-    while():
-        barcodes = pyzbar.decode(data)
-
-        # more than one detected barcode
-        for barcode in barcodes:
-            # bounding box location of barcode, drawn around barcode
-            (x, y, w, h) = barcode.rect
-            cv2.rectangle(data, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-            # convert barcode data from byte object to string
-            barcode_byte = barcode.data.decode("utf-8")
-            barcode_string = barcode.type
-
-            # show barcode type and data to user
-            print("The barcode {} {} was found".format(barcode_string, barcode_byte))
+import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 
-        # stops repeated prints
-        if len(barcodes) != 0:
-            break
+def retrieve_barcode(data):
+    data = cv2.imread(data)
 
-# construct request
-# to json
-# back to front end through ajax
-# useful info to user
-# pwa, css for responsive design
-# celinium (python automatic testing)
+    barcodes = pyzbar.decode(data)
+    parsed_barcodes = []
+    # more than one detected barcode
+    for barcode in barcodes:
+        # bounding box location of barcode, drawn around barcode
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(data, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # convert barcode data from byte object to string
+        barcode_byte = barcode.data.decode("utf-8")
+        barcode_string = barcode.type
+
+        # show barcode type and data to user
+        print("The barcode {} {} was found".format(barcode_string, barcode_byte))
+        parsed_barcodes.append([barcode_string, barcode_byte])
+
+
+        headers = {
+            # Request headers
+            'Ocp-Apim-Subscription-Key': 'cd24a9a50c9f4f0cb057c77c28a6cae7',
+        }
+
+        params = urllib.parse.urlencode({
+            # Request parameters
+            'gtin': "barcode_byte",
+        })
+        print(barcode_byte)
+        try:
+            print("cunt")
+            conn = http.client.HTTPSConnection('dev.tescolabs.com')
+            conn.request("GET", "/product/?%s" % params, "{description}", headers)
+            response = conn.getresponse()
+            data = response.read()
+            print(data)
+            conn.close()
+        except Exception as e:
+            print("error")
+
+    return parsed_barcodes
+
---
+++ web.py
@@ -0,0 +1,40 @@
+from recyclesaurs import barcode_find
+from flask import Flask, render_template, request
+import json
+import tempfile
+import os
+import base64
+
+app = Flask(__name__)
+DEBUG = True
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+
+@app.route('/image', methods=['POST', 'GET'])
+def text():
+    if request.is_json:
+        image = request.json.get("imageData")
+        if image:
+            print("Found Image")
+            # Remove the header
+            with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp:
+                stripped = image.replace("data:image/png;base64,", "")
+                decoded = base64.b64decode(stripped)
+                temp.write(decoded)
+                temp_path = temp.name
+            parsed_barcodes = barcode_find.retrieve_barcode(temp_path)
+
+            if not DEBUG:
+                os.remove(temp_path)
+            return json.dumps(parsed_barcodes), 200
+
+
+    return "OK"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, ssl_context=('server.crt', 'server.key'))
